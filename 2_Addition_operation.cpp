886817b77e7ab82d273886871591603753d91c22
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2022-09-06 15:40:52 +0530

    Create 2_Addition_operation.cpp

---
+++ 2_Addition_operation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Enter 2 numbers:" << endl;
+    cin >> a >> b;
+    c = a + b;
+    cout << "Addition of 2 numbers is: " << c << endl;
+
+    return 0;
+}
