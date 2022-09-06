5603c2c39b8da015ba7ebae2c4d8c5ef81ff040d
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2022-09-06 15:45:05 +0530

    Update 1_Hello_World.cpp

--- 1_Hello_World.cpp
+++ 1_Hello_World.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
+    string name;
     cout << "Hello World!";
-
+    //Showing to take input of a string
+    /*
+    cout << "What is your name?" << endl;
+    cin >> name;
+    //for giving out the output as full name we use getlin(cin, name); instead of cin >>
+    cout << "Hi, Mr/Miss "<< name << endl;
+    
     return 0;
 }
