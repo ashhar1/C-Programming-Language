4d790f8c392583fcfbb7e9c46f683ce8e7c1b05c
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2022-09-09 15:59:34 +0530

    Create 27_Factorial_of_a_number.cpp

---
+++ 27_Factorial_of_a_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, factorial = 1;
+    cout << "Enter the number: " << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+    }
+    cout << "The factorial of " << n << " is " << factorial << endl;
+
+    return 0;
+}
