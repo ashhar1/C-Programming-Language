a20bbab2bcb6f84f91f9cd47caba7616fd21823f
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2022-09-11 02:06:57 +0530

    Create 54_Average_of_a_matrix.cpp

---
+++ 54_Average_of_a_matrix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    float num[100], sum = 0.0, average;
+
+    cout << "Enter the numbers of element: " << endl;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << i + 1 << ". Enter number: ";
+        cin >> num[i];
+        sum = sum + num[i];
+    }
+
+    average = sum / n;
+    cout << "Average = " << average << endl;
+
+    return 0;
+}
