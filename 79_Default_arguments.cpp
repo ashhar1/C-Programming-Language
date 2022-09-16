bb407fffeaa26b274eb679d78ad8d5d2467451e9
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2022-09-17 02:04:39 +0530

    Update 79_Default_arguments.cpp

--- 79_Default_arguments.cpp
+++ 79_Default_arguments.cpp
@@ -5,11 +5,20 @@ int add(int x, int y, int z = 0)
 {
     return x + y + z;
 }
+int max(int a = 0, int b = 0, int c = 0)
+{
+    return a > b && a > c ? a : (b > c ? b : c);
+}
 int main()
 {
     cout << add(10, 5) << endl;
     cout << add(10, 5, 8) << endl;
     cout << add(17, 8, 0) << endl;
+
+    cout << max() << endl;
+    cout << max(10) << endl;
+    cout << max(10, 13) << endl;
+    cout << max(10, 13, 15) << endl;
 
     return 0;
 }
