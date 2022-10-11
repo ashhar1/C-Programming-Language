89a88089a6e6497840baa19ff8fe05af15acd464
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2022-10-12 02:16:51 +0530

    Create 122_Exception_handling_construct.cpp

---
+++ 122_Exception_handling_construct.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10, y = 0, z;
+
+    try
+    {
+        if (y == 0)
+            throw 1;
+        z = x / y;
+        cout << z << endl;
+    }
+
+    catch (int e)
+    {
+        cout << "Division by Zero " << e << endl;
+    }
+
+    cout << "Bye" << endl;
+
+    return 0;
+}
